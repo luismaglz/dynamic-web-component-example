5408b381b59dad9f1ddbd63321fbdadf0d14b6b8
Luis Gonzalez <luismaglz@live.com>
2019-08-17 08:15:41 -0600

    restarted project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
-import { Component, Injector, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { createCustomElement } from '@angular/elements';
-import { HelloComponent } from './hello.component';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-  name = 'Angular';
-
-  constructor(injector: Injector){
-    const myElement = createCustomElement(HelloComponent, {injector})
-    customElements.define('my-custom-element', HelloComponent, );
-    const test = document.createElement('my-custom-element') 
-    document.body.appendChild(test);
-  }
+export class AppComponent {
+  title = 'custom-element-example';
 }
--- src/app/hello-2.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  template: `<h1>Hello 2 {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
-})
-export class Hello2Component  {
-  @Input() name: string;
-}
